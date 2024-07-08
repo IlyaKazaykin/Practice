04d871a8a1fd7d49b3908ce1bbdb50ff3ee9ddde
IlyaKazaykin <ilya.kazaykin@mail.ru>
2024-07-08 15:07:54 +0400

    Add new files and folders

---
+++ stock_exchange/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    share_price = models.DecimalField(max_digits=10, decimal_places=2)
+    shares_available = models.IntegerField()
+    controlling_stake = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Buyer(models.Model):
+    id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=150)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.full_name
+
+class Deal(models.Model):
+    deal_date = models.DateField(primary_key=True)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    shares_quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"Deal {self.deal_date} - {self.buyer.full_name} - {self.company.name}"
